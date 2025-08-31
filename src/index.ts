dbe3744b3713f017afc98cfc78f9c7d991712e28
mrLuisFer <mrluisfeer@gmail.com>
2025-08-31 17:25:57 -0600

    feat: add index file to export hooks for easier access

---
+++ src/index.ts
@@ -0,0 +1,19 @@
+import { useCopyToClipboard } from './hooks/useCopyToClipboard.js';
+import { useIsClient } from './hooks/useIsClient.js';
+import { useLocalStorage } from './hooks/useLocalStorage.js';
+import { useMediaQuery } from './hooks/useMediaQuery.js';
+import { useToggle } from './hooks/useToggle.js';
+import { useUniqueIds } from './hooks/useUniqueIds.js';
+import { useHover } from './hooks/useHover.js';
+import { useDeviceResources } from './hooks/useDeviceResources.js';
+
+export {
+    useCopyToClipboard,
+    useIsClient,
+    useLocalStorage,
+    useMediaQuery,
+    useToggle,
+    useUniqueIds,
+    useHover,
+    useDeviceResources,
+};
